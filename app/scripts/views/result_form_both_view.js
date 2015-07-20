72b1a4bf7e2e12eef50aacba3939f02427c8c0dd
Griff <matthew@tendoto.com>
2015-07-20 00:49:32 -0400

    sunday1am

--- app/scripts/views/result_form_both_view.js
+++ app/scripts/views/result_form_both_view.js
@@ -10,14 +10,30 @@
 			this.render();
 			$('#viewContainer').html(this.$el);
 			
-			$('#add-hours-current').html(('$' + accounting.formatNumber(trainingCostPerHour * 20,2,',')));
-			$('#add-hours-edify').html(('$' + accounting.formatNumber(edifyPerHour * 20,2,',')));
-			$('#return2').html(accounting.formatNumber(returnOnInvestment1 * 100,2,',') + '%');
-			$('#total-cost').html('$' + accounting.formatNumber(edifyAnually,2,','));
-			$('#cost-employee').html('$' + accounting.formatNumber(trainingCostPerEmployee,2,','));
-			$('#savings').html('$' + accounting.formatNumber((trainingCost - edifyAnually),2,','));
-			$('#edify-cost-hour').html('$' + accounting.formatNumber(edifyPerHour,2,','));
-			$('#return').html(accounting.formatNumber(returnOnInvestment1 * 100,2,',') + '%');
+			var $modal = $('.js-loading-bar'),
+		        $bar = $modal.find('.progress-bar');
+		    
+		    $modal.modal('show');
+
+		    setTimeout(function() {
+		      $modal.modal('hide'); 
+		      $bar.removeClass('animate');
+		      //$modal.modal('hide');   
+			  $('#total-cost').html('$' + accounting.formatNumber(edifyAnually,2,','));
+			  $('#cost-employee').html('$' + accounting.formatNumber(trainingCostPerEmployee,2,','));
+			  $('#savings').html('$' + accounting.formatNumber((trainingCost - edifyAnually),2,','));
+			  $('#edify-cost-hour').html('$' + accounting.formatNumber(edifyPerHour,2,','));
+			  $('#return').html(accounting.formatNumber(returnOnInvestment1 * 100,2,',') + '%');
+			  
+			  $('#add-hours-current').html('$' + accounting.formatNumber(trainingCostPerHour * 20,2,','));
+  			$('#add-hours-edify').html('$' + accounting.formatNumber(edifyPerHour * 20,2,','));
+  			
+  			$('#return').html(accounting.formatNumber(returnOnInvestment1 * 100,2,',') + '%');
+			  
+		    }, 1500);
+			
+			
+			
 		},
 		render: function(){
 			this.$el.html(this.template);
